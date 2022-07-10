f4c14ce41ecbb72ccc9d393f6179974313830849
yuvalbe <yuvalbe@hailo.ai>
2022-07-10 15:45:07 +0000

    modified gstthreadmonitor.c

--- plugins/tracers/gstthreadmonitor.c
+++ plugins/tracers/gstthreadmonitor.c
@@ -33,7 +33,7 @@
 #include "gstthreadmonitorcompute.h"
 #include "gstctf.h"
 
-GST_DEBUG_CATEGORY_STATIC (gst_thread_monitor_debug);
+GST_DEBUG_CATEGORY_STATIC(gst_thread_monitor_debug);
 #define GST_CAT_DEFAULT gst_thread_monitor_debug
 
 struct _GstThreadMonitorTracer
@@ -44,10 +44,10 @@ struct _GstThreadMonitorTracer
 };
 
 #define _do_init \
-    GST_DEBUG_CATEGORY_INIT (gst_thread_monitor_debug, "threadmonitor", 0, "threadmonitor tracer");
+  GST_DEBUG_CATEGORY_INIT(gst_thread_monitor_debug, "threadmonitor", 0, "threadmonitor tracer");
 
-G_DEFINE_TYPE_WITH_CODE (GstThreadMonitorTracer, gst_thread_monitor_tracer,
-    GST_TYPE_PERIODIC_TRACER, _do_init);
+G_DEFINE_TYPE_WITH_CODE(GstThreadMonitorTracer, gst_thread_monitor_tracer,
+                        GST_TYPE_PERIODIC_TRACER, _do_init);
 
 static GstTracerRecord *tr_threadmonitor;
 
@@ -66,31 +66,31 @@ static const gchar threadmonitor_metadata_event_footer[] = "\
 static const gchar floating_point_event_field[] =
     "        floating_point { exp_dig = %lu; mant_dig = %d; byte_order = le; align = 8; } _cpu%d;\n";
 
-static void threadmonitor_dummy_bin_add_post (GObject * obj, GstClockTime ts,
-    GstBin * bin, GstElement * element, gboolean result);
-static gboolean thread_monitor_thread_func (GstPeriodicTracer * tracer);
-static void create_metadata_event (GstPeriodicTracer * tracer);
-static void reset_counters (GstPeriodicTracer * tracer);
+static void threadmonitor_dummy_bin_add_post(GObject *obj, GstClockTime ts,
+                                             GstBin *bin, GstElement *element, gboolean result);
+static gboolean thread_monitor_thread_func(GstPeriodicTracer *tracer);
+static void create_metadata_event(GstPeriodicTracer *tracer);
+// static void reset_counters (GstPeriodicTracer * tracer);
 
 static void
-threadmonitor_dummy_bin_add_post (GObject * obj, GstClockTime ts,
-    GstBin * bin, GstElement * element, gboolean result)
+threadmonitor_dummy_bin_add_post(GObject *obj, GstClockTime ts,
+                                 GstBin *bin, GstElement *element, gboolean result)
 {
   return;
 }
 
-static void
-reset_counters (GstPeriodicTracer * tracer)
-{
-  GstThreadMonitorTracer *self;
+// static void
+// reset_counters (GstPeriodicTracer * tracer)
+// {
+//   GstThreadMonitorTracer *self;
 
-  self = GST_THREAD_MONITOR_TRACER (tracer);
+//   self = GST_THREAD_MONITOR_TRACER (tracer);
 
-  gst_thread_monitor_init (&(self->thread_monitor));
-}
+//   gst_thread_monitor_init (&(self->thread_monitor));
+// }
 
 static gboolean
-thread_monitor_thread_func (GstPeriodicTracer * tracer)
+thread_monitor_thread_func(GstPeriodicTracer *tracer)
 {
   GstThreadMonitorTracer *self;
   GstThreadMonitor *thread_monitor;
@@ -98,7 +98,7 @@ thread_monitor_thread_func (GstPeriodicTracer * tracer)
   // gint cpu_id;
   gint cpu_load_len;
 
-  self = GST_THREAD_MONITOR_TRACER (tracer);
+  self = GST_THREAD_MONITOR_TRACER(tracer);
 
   thread_monitor = &self->thread_monitor;
 
@@ -107,18 +107,19 @@ thread_monitor_thread_func (GstPeriodicTracer * tracer)
   // cpu_load_len = THREAD_MONITOR_ARRAY_LENGTH (cpu_usage);
   cpu_load_len = 12;
 
-  gst_thread_monitor_compute (thread_monitor);
+  gst_thread_monitor_compute(thread_monitor);
 
   // for (cpu_id = 0; cpu_id < cpu_load_len; ++cpu_id) {
   //   gst_tracer_record_log (tr_threadmonitor, cpu_id, cpu_load[cpu_id]);
   // }
-  do_print_threadmonitor_event (THREADMONITOR_EVENT_ID, cpu_load_len, cpu_load);
+  gst_tracer_record_log(tr_threadmonitor, 1, 0.5);
+  do_print_threadmonitor_event(THREADMONITOR_EVENT_ID, cpu_load_len, cpu_load);
 
   return TRUE;
 }
 
 static void
-create_metadata_event (GstPeriodicTracer * tracer)
+create_metadata_event(GstPeriodicTracer *tracer)
 {
   // GstThreadMonitorTracer *self;
   gint cpu_num;
@@ -134,84 +135,93 @@ create_metadata_event (GstPeriodicTracer * tracer)
   // cpu_num = self->cpu_usage.cpu_num;
   cpu_num = 12;
   event_header =
-      g_strdup_printf (threadmonitor_metadata_event_header, THREADMONITOR_EVENT_ID, 0);
+      g_strdup_printf(threadmonitor_metadata_event_header, THREADMONITOR_EVENT_ID, 0);
 
-  str_size = strlen (event_header);
+  str_size = strlen(event_header);
 
   /* Compute event description size
    * size = header + fields + footer
    */
   mem_size =
-      str_size + cpu_num * sizeof (floating_point_event_field) +
-      sizeof (threadmonitor_metadata_event_footer);
-  mem_start = g_malloc (mem_size);
+      str_size + cpu_num * sizeof(floating_point_event_field) +
+      sizeof(threadmonitor_metadata_event_footer);
+  mem_start = g_malloc(mem_size);
   mem = mem_start;
 
   /* Add event header */
-  mem += g_strlcpy (mem, event_header, mem_size);
+  mem += g_strlcpy(mem, event_header, mem_size);
   mem_size -= str_size;
 
   /* Add event fields */
-  for (msg_id = 0; msg_id < cpu_num; ++msg_id) {
+  for (msg_id = 0; msg_id < cpu_num; ++msg_id)
+  {
     /* floating point field definition:
      * http://diamon.org/ctf/#spec4.1.7
      */
-    number_of_bytes = g_snprintf (mem,
-        mem_size,
-        floating_point_event_field,
-        (unsigned long) (sizeof (gfloat) * CHAR_BIT - FLT_MANT_DIG),
-        FLT_MANT_DIG, msg_id);
+    number_of_bytes = g_snprintf(mem,
+                                 mem_size,
+                                 floating_point_event_field,
+                                 (unsigned long)(sizeof(gfloat) * CHAR_BIT - FLT_MANT_DIG),
+                                 FLT_MANT_DIG, msg_id);
 
     mem += number_of_bytes;
     mem_size -= number_of_bytes;
   }
 
   /* Add event footer */
-  g_strlcpy (mem, threadmonitor_metadata_event_footer, mem_size);
+  g_strlcpy(mem, threadmonitor_metadata_event_footer, mem_size);
 
   /* Add event in metadata file */
-  add_metadata_event_struct (mem_start);
+  add_metadata_event_struct(mem_start);
   /* Free allocated memory */
-  g_free (mem_start);
-  g_free (event_header);
+  g_free(mem_start);
+  g_free(event_header);
 }
 
 static void
-gst_thread_monitor_tracer_class_init (GstThreadMonitorTracerClass * klass)
+gst_thread_monitor_tracer_class_init(GstThreadMonitorTracerClass *klass)
 {
   GstPeriodicTracerClass *tracer_class;
 
-  tracer_class = GST_PERIODIC_TRACER_CLASS (klass);
+  tracer_class = GST_PERIODIC_TRACER_CLASS(klass);
 
-  tracer_class->timer_callback = GST_DEBUG_FUNCPTR (thread_monitor_thread_func);
-  tracer_class->reset = GST_DEBUG_FUNCPTR (reset_counters);
-  tracer_class->write_header = GST_DEBUG_FUNCPTR (create_metadata_event);
+  tracer_class->timer_callback = GST_DEBUG_FUNCPTR(thread_monitor_thread_func);
+  // tracer_class->reset = GST_DEBUG_FUNCPTR (reset_counters);
+  tracer_class->write_header = GST_DEBUG_FUNCPTR(create_metadata_event);
 
-  tr_threadmonitor = gst_tracer_record_new ("threadmonitor.class",
-      "number", GST_TYPE_STRUCTURE, gst_structure_new ("value",
-          "type", G_TYPE_GTYPE, G_TYPE_UINT,
-          "description", G_TYPE_STRING, "Core number",
-          "flags", GST_TYPE_TRACER_VALUE_FLAGS,
-          GST_TRACER_VALUE_FLAGS_AGGREGATED, "min", G_TYPE_UINT, 0, "max",
-          G_TYPE_UINT, CPU_NUM_MAX, NULL), "load", GST_TYPE_STRUCTURE,
-      gst_structure_new ("value", "type", G_TYPE_GTYPE, G_TYPE_DOUBLE,
-          "description", G_TYPE_STRING, "Core load percentage [%]", "flags",
-          GST_TYPE_TRACER_VALUE_FLAGS, GST_TRACER_VALUE_FLAGS_AGGREGATED, "min",
-          G_TYPE_DOUBLE, 0.0f, "max", G_TYPE_DOUBLE, 100.0f, NULL), NULL);
+  tr_threadmonitor = gst_tracer_record_new("threadmonitor.class",
+                                           "name", GST_TYPE_STRUCTURE,
+                                           gst_structure_new("value", "type", G_TYPE_GTYPE, G_TYPE_STRING,
+                                                             "description", G_TYPE_STRING,
+                                                             "thread name", "flags", GST_TYPE_TRACER_VALUE_FLAGS,
+                                                             GST_TRACER_VALUE_FLAGS_AGGREGATED, NULL),
+                                           "cpu usage",
+                                           GST_TYPE_STRUCTURE,
+                                           gst_structure_new("value", "type", G_TYPE_GTYPE, G_TYPE_DOUBLE,
+                                                             "description", G_TYPE_STRING, "CPU usage of thread percentage [%]", "flags",
+                                                             GST_TYPE_TRACER_VALUE_FLAGS, GST_TRACER_VALUE_FLAGS_AGGREGATED, "min",
+                                                             G_TYPE_DOUBLE, 0.0f, "max", G_TYPE_DOUBLE, 100.0f, NULL),
+
+                                           "memory usage",
+                                           GST_TYPE_STRUCTURE,
+                                           gst_structure_new("value", "type", G_TYPE_GTYPE, G_TYPE_DOUBLE,
+                                                             "description", G_TYPE_STRING, "Memory usage of thread percentage [%]", "flags",
+                                                             GST_TYPE_TRACER_VALUE_FLAGS, GST_TRACER_VALUE_FLAGS_AGGREGATED, "min",
+                                                             G_TYPE_DOUBLE, 0.0f, "max", G_TYPE_DOUBLE, 100.0f, NULL),
+                                           NULL);
 }
 
 static void
-gst_thread_monitor_tracer_init (GstThreadMonitorTracer * self)
+gst_thread_monitor_tracer_init(GstThreadMonitorTracer *self)
 {
 
   GstThreadMonitor *thread_monitor;
-
   thread_monitor = &self->thread_monitor;
   self->pid = getpid();
-  gst_thread_monitor_init (thread_monitor);
+  gst_thread_monitor_init(thread_monitor);
   // cpu_usage->cpu_array_sel = FALSE;
 
   /* Register a dummy hook so that the tracer remains alive */
-  gst_tracing_register_hook (GST_TRACER (self), "bin-add-post",
-      G_CALLBACK (threadmonitor_dummy_bin_add_post));
+  gst_tracing_register_hook(GST_TRACER(self), "bin-add-post",
+                            G_CALLBACK(threadmonitor_dummy_bin_add_post));
 }
