d1ce1c0b09aed8a007b64f9107f1315bc8794121
yuvalbe <yuvalbe@hailo.ai>
2022-07-12 17:00:01 +0000

    printing only some of the threads

--- plugins/tracers/gstthreadmonitorcompute.c
+++ plugins/tracers/gstthreadmonitorcompute.c
@@ -51,9 +51,14 @@ void gst_thread_monitor_compute(GstTracerRecord *tr_threadmonitor, GstThreadMoni
   gint thread_name_loc;
   gint thread_cpu_usage_loc;
   gint thread_memory_usage_loc;
+  int counter;
 
   char path[4096];
   char *path_stripped;
+
+  char output_top[4096];
+  char *output_top_stripped;
+
   char columns[4096];
   gint num_columns = 0;
   g_return_if_fail(thread_monitor);
@@ -81,8 +86,7 @@ void gst_thread_monitor_compute(GstTracerRecord *tr_threadmonitor, GstThreadMoni
       token = strtok(NULL, " ");
     }
   }
-  // print num columns
-  //  printf("num_columns: %d\n", num_columns);
+
   tokens = g_strsplit(columns, " ", num_columns);
 
   thread_name_loc = -1;
@@ -115,22 +119,26 @@ void gst_thread_monitor_compute(GstTracerRecord *tr_threadmonitor, GstThreadMoni
     GST_WARNING("Failed to run command");
     return;
   }
-  while (fgets(path, PATH_MAX, fp) != NULL)
+  printf("*************PRINTING RECORDS******************\n");
+  counter = 0;
+  while (fgets(output_top, PATH_MAX, fp) != NULL)
   {
-    path_stripped = g_strstrip(path);
-    printf("path stripped: %s",path_stripped);
-    tokens = g_strsplit(path_stripped, " ", num_columns);
+    counter++;
+    output_top_stripped = g_strstrip(output_top);
+    printf("output_top_stripped:%s\n", output_top_stripped);
+    tokens = g_strsplit(output_top_stripped, " ", num_columns);
     *thread_name = tokens[thread_name_loc];
     *thread_cpu_usage = tokens[thread_cpu_usage_loc];
     *thread_memory_usage = tokens[thread_memory_usage_loc];
 
     // printf("THREAD NAME: %s\n", *thread_name);
-
-    // gst_tracer_record_log(tr_threadmonitor, *thread_name, atof(*thread_cpu_usage), atof(*thread_memory_usage));
+    gst_tracer_record_log(tr_threadmonitor, *thread_name, atof(*thread_cpu_usage), atof(*thread_memory_usage));
     *thread_name = NULL;
     *thread_cpu_usage = NULL;
     *thread_memory_usage = NULL;
   }
+  //print counter
+  printf("COUNTER:%d\n", counter);
   pclose(fp);
   g_free(command);
 }
