71f6586e0a03afecb1ec268d70385e3dd2cd1c12
yuvalbe <yuvalbe@hailo.ai>
2022-07-13 15:22:32 +0000

    cleaed

--- plugins/tracers/gstthreadmonitorcompute.c
+++ plugins/tracers/gstthreadmonitorcompute.c
@@ -47,80 +47,72 @@ void gst_thread_monitor_compute(GstTracerRecord *tr_threadmonitor, GstThreadMoni
   gchar *command;
   char **tokens;
 
-  // gint thread_name_loc;
-  // gint thread_cpu_usage_loc;
-  // gint thread_memory_usage_loc;
+  gint thread_name_loc;
+  gint thread_cpu_usage_loc;
+  gint thread_memory_usage_loc;
   int counter;
   size_t len = 0;
   char *line = NULL;
+  char *strip_line = NULL;
   ssize_t read;
   // int k;
 
   // char columns[4096];
-  // gint num_columns = 0;
+  gint num_columns = 0;
 
-  // char *columns = NULL;
-  // gchar *token;
-  // gchar *colums_command;
+  char *columns = NULL;
+  gchar *token;
+  gchar *colums_command;
 
   g_return_if_fail(thread_monitor);
 
-  // colums_command = g_strdup_printf("top -H -p %d -n 1 | grep PID | tr -s ' ' | sed -e 's/\x1b\[[0-9;]*m//g'", getpid());
+  colums_command = g_strdup_printf("top -b -H-p %d -n 1 | grep PID | tr -s ' ' | sed -e 's/\x1b\[[0-9;]*m//g'", getpid());
 
-  // fp = popen(colums_command, "r");
+  fp = popen(colums_command, "r");
 
-  // if (fp == NULL)
-  // {
-  //   GST_WARNING("Failed to run command");
-  //   return;
-  // }
-  // if (getline(&line, &len, fp) != -1)
-  // {
-  //   stripped_line = g_strstrip(line);
-  //   columns = (char *)malloc(strlen(stripped_line) + 1);
-  //   strcpy(columns, stripped_line);
-  //   token = strtok(stripped_line, " ");
-  //   while (token != NULL)
-  //   {
-  //     num_columns++;
-  //     token = strtok(NULL, " ");
-  //   }
-  // }
-  // tokens = g_strsplit(columns, " ", num_columns);
+  if (fp == NULL)
+  {
+    GST_WARNING("Failed to run command");
+    return;
+  }
+  if (getline(&line, &len, fp) != -1)
+  {
+    strip_line = g_strstrip(line);
+    columns = (char *)malloc(strlen(strip_line) + 1);
+    strcpy(columns, strip_line);
+    token = strtok(strip_line, " ");
+    while (token != NULL)
+    {
+      num_columns++;
+      token = strtok(NULL, " ");
+    }
+  }
+  tokens = g_strsplit(columns, " ", num_columns);
 
-  // thread_name_loc = -1;
-  // thread_cpu_usage_loc = -1;
-  // thread_memory_usage_loc = -1;
-  // for (int i = 0; i < num_columns; i++)
-  // {
-  //   if (strcmp(tokens[i], "COMMAND") == 0)
-  //   {
-  //     thread_name_loc = i;
-  //   }
-  //   else if (strcmp(tokens[i], "%CPU") == 0)
-  //   {
-  //     thread_cpu_usage_loc = i;
-  //   }
-  //   else if (strcmp(tokens[i], "%MEM") == 0)
-  //   {
-  //     thread_memory_usage_loc = i;
-  //   }
-  // }
-  // free(columns);
-  // pclose(fp);
-  // g_free(colums_command);
-  // print all loc
-  // printf("thread_name_loc: %d\n", thread_name_loc);
-  // printf("thread_cpu_usage_loc: %d\n", thread_cpu_usage_loc);
-  // printf("thread_memory_usage_loc: %d\n", thread_memory_usage_loc);
+  thread_name_loc = -1;
+  thread_cpu_usage_loc = -1;
+  thread_memory_usage_loc = -1;
+  for (int i = 0; i < num_columns; i++)
+  {
+    // printf("TOKEN: %s\n", tokens[i]);
+    if (strcmp(tokens[i], "COMMAND") == 0)
+    {
+      thread_name_loc = i;
+    }
+    else if (strcmp(tokens[i], "%CPU") == 0)
+    {
+      thread_cpu_usage_loc = i;
+    }
+    else if (strcmp(tokens[i], "%MEM") == 0)
+    {
+      thread_memory_usage_loc = i;
+    }
+  }
+  free(columns);
+  pclose(fp);
+  g_free(colums_command);
 
-  // thread_name_loc = 11;
-  // thread_cpu_usage_loc = 8;
-  // thread_memory_usage_loc = 9;
-
-  // command = g_strdup_printf("top -H -p %d -n 1 | sed -n '/PID/,/^$/p' | tail -n +2 | tr -s ' ' | grep src | sed -e 's/\x1b\[[0-9;]*m//g'", getpid());
-  command = g_strdup_printf("top -H -b -p %d -n 1 | sed -n '/PID/,/^$/p' | tail -n +2 | tr -s ' ' | grep src | sed -e 's/\x1b\[[0-9;]*m//g' | awk '{print $12,$9,$10}'", getpid());
-  // command = g_strdup_printf("cat /local/workspace/tappas/top_output_3");
+  command = g_strdup_printf("top -H -b -p %d -n 1 | sed -n '/PID/,/^$/p' | tail -n +2 | tr -s ' ' | grep src | sed -e 's/\x1b\[[0-9;]*m//g' | awk '{print $%d,$%d,$%d}'", getpid(), thread_name_loc, thread_cpu_usage_loc, thread_memory_usage_loc);
   fp = popen(command, "r");
   if (fp == NULL)
   {
@@ -130,14 +122,9 @@ void gst_thread_monitor_compute(GstTracerRecord *tr_threadmonitor, GstThreadMoni
   }
   counter = 0;
 
-  // printf("PID: %d\n", getpid());
   while ((read = getline(&line, &len, fp)) != -1)
   {
-    // line[0]=' ';
-    line = line + 3;
-    printf("***PID %d\n",getpid());
-    printf("***PRINTING ASCI\n");
-
+    // printf("***PID %d\n", getpid());
 
     counter++;
     tokens = g_strsplit(line, " ", 3);
@@ -153,8 +140,6 @@ void gst_thread_monitor_compute(GstTracerRecord *tr_threadmonitor, GstThreadMoni
     len = 0;
   }
 
-  // printf("Retrieved line of length %zu:\n", read);
-  // print counter
   printf("COUNTER:%d\n", counter);
   pclose(fp);
   g_free(command);
