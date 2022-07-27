f2d781060aa5e2d3484b297b12ab7cee4f68c0f3
yuvalbe <yuvalbe@hailo.ai>
2022-07-27 11:21:07 +0000

    Wip

---
+++ scripts/graphics/split_traces.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+split_traces_dir=$1
+
+mkdir -p $split_traces_dir
+
+awk '!/gsttracer.c|gsttracerrecord.c/' $GST_DEBUG_FILE > $split_traces_dir/filtered.txt
+
+#read value of environment variable GST_TRACERS and split it into an array of tracers
+IFS=';' read -ra tracers <<< "$GST_TRACERS"
+
+for trace in "${tracers[@]}"
+do
+    #check if trace is not graphic and if not, do next line
+    if [[ $trace == "graphic" ]]; then
+        continue
+    fi
+    
+    cat $split_traces_dir/filtered.txt | grep $trace > $split_traces_dir/$trace.log
+done
+
+rm $split_traces_dir/filtered.txt
+
