bd1e3b9a3d90bb350452522ef54da898b7daa897
yuvalbe <yuvalbe@hailo.ai>
2022-08-02 11:31:14 +0000

    need to add offset counter for every stream id

--- plugins/tracers/gstnumerator.c
+++ plugins/tracers/gstnumerator.c
@@ -25,6 +25,7 @@
 
 #include "gstnumerator.h"
 #include "gstctf.h"
+#include <stdio.h>
 
 GST_DEBUG_CATEGORY_STATIC(gst_numerator_debug);
 #define GST_CAT_DEFAULT gst_numerator_debug
@@ -40,20 +41,81 @@ struct _GstNumeratorTracer
 G_DEFINE_TYPE_WITH_CODE(GstNumeratorTracer, gst_numerator_tracer,
                         GST_SHARK_TYPE_TRACER, _do_init);
 
+static GstElement *
+get_parent_element(GstPad *pad)
+{
+    GstElement *element;
+    GstObject *parent;
+    GstObject *child = GST_OBJECT(pad);
+
+    do
+    {
+        parent = GST_OBJECT_PARENT(child);
+
+        if (GST_IS_ELEMENT(parent))
+            break;
+
+        child = parent;
+
+    } while (GST_IS_OBJECT(child));
+
+    element = gst_pad_get_parent_element(GST_PAD(child));
+
+    return element;
+}
+
+static gboolean
+is_decoder(GstElement *element)
+{
+    static GstElementFactory *decodebin_factory = NULL;
+    static GstElementFactory *uridecodebin3_factory = NULL;
+    static GstElementFactory *avdec_factory = NULL;
+    GstElementFactory *efactory;
+
+    g_return_val_if_fail(element, FALSE);
+
+    /* Find the decoder factory that is going to be compared against
+       the element under inspection to see if it is a decoder */
+    decodebin_factory = gst_element_factory_find("decodebin");
+    uridecodebin3_factory = gst_element_factory_find("uridecodebin3");
+    avdec_factory = gst_element_factory_find("avdec_h264");
+
+    efactory = gst_element_get_factory(element);
+
+    return (efactory == uridecodebin3_factory || efactory == avdec_factory || efactory == decodebin_factory);
+}
+
 static void gst_numerator_buffer_pre(GObject *self, GstClockTime ts,
                                      GstPad *pad, GstBuffer *buffer);
+
+gint64 i = 0;
 static void
 gst_numerator_buffer_pre(GObject *self, GstClockTime ts, GstPad *pad,
                          GstBuffer *buffer)
 {
-    buffer->offset = 0;
-}
+    GstElement *element;
+    gchar *stream_id;
+    element = get_parent_element(pad);
 
+    if (!is_decoder(element))
+    {
+        return;
+    }
+
+    stream_id = gst_pad_get_stream_id(pad);
+
+    printf("stream_id: %s\n", stream_id);
+
+    buffer->offset = i;
+    i++;
+    g_free(stream_id);
+}
 
 /* tracer class */
 static void
 gst_numerator_tracer_class_init(GstNumeratorTracerClass *klass)
-{}
+{
+}
 
 static void
 gst_numerator_tracer_init(GstNumeratorTracer *self)
