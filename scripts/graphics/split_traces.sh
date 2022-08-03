f45cf75d1f6aa764afdf0cb576deca6bc2756e25
yuvalbe <yuvalbe@hailo.ai>
2022-08-03 11:01:52 +0000

    cleaned split traces

--- scripts/graphics/split_traces.sh
+++ scripts/graphics/split_traces.sh
@@ -2,7 +2,6 @@
 set -e
 
 split_traces_dir=$1
-
 mkdir -p $split_traces_dir
 
 #read value of environment variable GST_TRACERS and split it into an array of tracers, then loop through the array and create a file for each tracer
@@ -15,8 +14,5 @@ do
     fi
     
     cat $GST_DEBUG_FILE | awk '!/gsttracer.c|gsttracerrecord.c/' | grep $trace | tr -s ' ' > $split_traces_dir/$trace.log
-    
 
 done
-
-
