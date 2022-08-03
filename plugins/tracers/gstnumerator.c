1b23047e1fceb14af64cac142aed014d9304304d
yuvalbe <yuvalbe@hailo.ai>
2022-08-03 13:29:03 +0000

    moved to init

--- plugins/tracers/gstnumerator.c
+++ plugins/tracers/gstnumerator.c
@@ -28,7 +28,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
 GST_DEBUG_CATEGORY_STATIC(gst_numerator_debug);
 #define GST_CAT_DEFAULT gst_numerator_debug
 #define MAX_STREAMS 256
@@ -43,6 +42,8 @@ struct _GstNumeratorTracer
 
 G_DEFINE_TYPE_WITH_CODE(GstNumeratorTracer, gst_numerator_tracer,
                         GST_SHARK_TYPE_TRACER, _do_init);
+
+static GstElementFactory *avdec_factory = NULL;
 
 static GstElement *
 get_parent_element(GstPad *pad)
@@ -67,21 +68,13 @@ get_parent_element(GstPad *pad)
     return element;
 }
 
-static gboolean is_decoder(GstElement * element);
+static gboolean is_decoder(GstElement *element);
 
 static gboolean is_decoder(GstElement *element)
 {
-    static GstElementFactory *avdec_factory = NULL;
     GstElementFactory *efactory;
-
     g_return_val_if_fail(element, FALSE);
-
-    /* Find the decoder factory that is going to be compared against
-       the element under inspection to see if it is a decoder */
-    avdec_factory = gst_element_factory_find("avdec_h264");
-
     efactory = gst_element_get_factory(element);
-
     return (efactory == avdec_factory);
 }
 
@@ -131,23 +124,26 @@ static void gst_numerator_buffer_pre(GObject *self, GstClockTime ts, GstPad *pad
             GST_ERROR("Too many streams");
             return;
         }
-        strcpy(stream_ids[stream_count-1], stream_id);
-        offsets[stream_count-1]++;
-        buffer->offset = offsets[stream_count-1];
+        strcpy(stream_ids[stream_count - 1], stream_id);
+        offsets[stream_count - 1]++;
+        buffer->offset = offsets[stream_count - 1];
     }
     g_free(stream_id);
     gst_object_unref(element);
-
 }
 
 /* tracer class */
 static void
-gst_numerator_tracer_class_init(GstNumeratorTracerClass *klass){}
+gst_numerator_tracer_class_init(GstNumeratorTracerClass *klass) {}
 
 static void
 gst_numerator_tracer_init(GstNumeratorTracer *self)
 {
     GstSharkTracer *tracer = GST_SHARK_TRACER(self);
+
+    /* Find the decoder factory that is going to be compared against
+    the element under inspection to see if it is a decoder */
+    avdec_factory = gst_element_factory_find("avdec_h264");
 
     gst_shark_tracer_register_hook(tracer, "pad-push-pre",
                                    G_CALLBACK(gst_numerator_buffer_pre));
