7dbb2dc7be3f1d08ab33985d036e653d4aedc774
yuvalbe <yuvalbe@hailo.ai>
2022-08-03 11:00:14 +0000

    cleaned split traces

--- scripts/graphics/split_traces.sh
+++ scripts/graphics/split_traces.sh
@@ -5,25 +5,18 @@ split_traces_dir=$1
 
 mkdir -p $split_traces_dir
 
-# awk '!/gsttracer.c|gsttracerrecord.c/' $GST_DEBUG_FILE > $split_traces_dir/filtered.txt
-
 #read value of environment variable GST_TRACERS and split it into an array of tracers
 IFS=';' read -ra tracers <<< "$GST_TRACERS"
 
 for trace in "${tracers[@]}"
 do
-    #check if trace is not graphic and if not, do next line
     if [[ $trace == "graphic" ]]; then
         continue
     fi
     
-    
     cat $GST_DEBUG_FILE | awk '!/gsttracer.c|gsttracerrecord.c/' | grep $trace | tr -s ' ' > $split_traces_dir/$trace.log
     
-    #replace any number of spaces with single space in every trace log file
 
-    # cat $split_traces_dir/filtered.txt | grep $trace > $split_traces_dir/$trace.log
 done
 
-# rm $split_traces_dir/filtered.txt
 
